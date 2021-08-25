412fd7e30e92bc6f2c458fde4c132e94bec66dde
Nancy Boktor <nancyboktor1@gmail.com>
2021-08-25 16:23:02 -0700

    Second step of the stretch activity Loopy-Lighthouse2

--- Loopy-Lighthouse2-StretchActivityNo1.js
+++ Loopy-Lighthouse2-StretchActivityNo1.js
@@ -1,25 +1,28 @@
 // range should be an array of 2 numbers representing the start and end values for the loop. 
-function loopyLighthouse (range){
+function loopyLighthouse (range, multiples){
  // We will declare a new array contains all numbers from range[0] to [1].
  let newArray = [];
  // Using while loop to count from range[0]to[1].
-    while (range[0] <= range[1]){
+    while (range[0] <= range[1]){ 
 // Pushing all numbers to the newArray.
-        newArray.push(range[0]);
+     newArray.push(range[0]);
+     let i = 0;
+  if ((newArray[i] % multiples[0] === 0) && (newArray % multiples[1] === 0)){
+     newArray[i] = "BattyBeacon";
+    return newArray;
+            
+  } else if (newArray[i] % multiples[0] === 0){
+     newArray[i] = "Batty";
+    return newArray;
+            
+  } else if (newArray[i] % multiples[1] === 0){
+     newArray[i] = "Beacon";
+    return newArray;
+    }
 // Increase the value of range[0] by one and starting a new around with while loop.
-        range[0]++;
+    range[0]++;
     } 
 return newArray;
-}
+} 
 
-
-
-
-
-
-
-
-
-
-
-console.log (loopyLighthouse([15, 90]));
+console.log (loopyLighthouse([15, 90], [2, 5]));
