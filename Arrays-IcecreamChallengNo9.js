10ce293ccfa50ca36c465c9fdba825baf5025e82
Nancy Boktor <nancyboktor1@gmail.com>
2021-08-25 13:19:12 -0700

    ice cream flavours.

---
+++ Arrays-IcecreamChallengNo9.js
@@ -0,0 +1,13 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");         // Add a flavour "root beer"
+console.log (iceCreamFlavours[0]);          // Get the first flavour.
+console.log (iceCreamFlavours[5]);          // Get the last flavour.
+console.log (iceCreamFlavours.length);      // Knowing the total nomber of flavours.
+
+
+
+
